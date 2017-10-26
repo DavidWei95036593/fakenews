dbbee527872f37d6f3aa6de44bbda5aa55fd6464
DavidWei95036593 <dw36593@pausd.us>
2017-10-26 08:21:44 -0700

    make a second change to a minecraft picture

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var imge = document.querySelectorAll("img");
-for (var i = 0; i < imge.length; i ++){
+for (var i = 1; i < imge.length; i +=2){
   imge[i].src ="http://cdn3-www.cattime.com/assets/uploads/2012/05/socializing-kitten-300x200.jpg";
 }
-
+for(var i = 0; i <imge.length; i+=2){
+	imge[i].src="http://orig14.deviantart.net/b291/f/2013/239/e/d/ssupersteve_face_by_frozen2-d6k0232.png";
+}
 
 var title = document.querySelectorAll("h1");
 title;
