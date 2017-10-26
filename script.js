5e6f3262310e3b0334392f0bbf5c744c809b1c5f
DavidWei95036593 <dw36593@pausd.us>
2017-10-26 08:19:38 -0700

    change all imge to a cat

---
+++ script.js
@@ -0,0 +1,8 @@
+var imge = document.querySelectorAll("img");
+for (var i = 0; i < imge.length; i ++){
+  imge[i].src ="http://cdn3-www.cattime.com/assets/uploads/2012/05/socializing-kitten-300x200.jpg";
+}
+
+
+var title = document.querySelectorAll("h1");
+title;
